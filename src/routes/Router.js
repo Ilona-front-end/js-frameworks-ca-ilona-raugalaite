39dedbaa920736574f8aca229d1184c645c0453c
Ilona R <ilona.raugalaite@yahoo.co.uk>
2023-03-28 23:04:53 +0200

    contact page sceleton

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "../components/pages/Homepage";
+import ContactUsPage from "../components/pages/ContactUsPage";
 import PageDoesNotExist from "../components/pages/PageDoesNotExist";
 
 function Router() {
@@ -8,6 +9,7 @@ function Router() {
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="contact" element={<ContactUsPage />} />
         <Route path="*" element={<PageDoesNotExist />} />
       </Routes>
     </>
