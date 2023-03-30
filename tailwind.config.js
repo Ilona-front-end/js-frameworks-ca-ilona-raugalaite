0a587661ecfb939dbd927d2b9ad829418c2b541e
Ilona R <ilona.raugalaite@yahoo.co.uk>
2023-03-30 17:52:42 +0200

    components/Header.js sceleton

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/aspect-ratio', '@tailwindcss/forms')],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms')
+  ],
 };
