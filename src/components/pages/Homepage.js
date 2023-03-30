6899829d9bc03f43817969e5a3ae95e5ec15be1b
Ilona R <ilona.raugalaite@yahoo.co.uk>
2023-03-30 20:40:59 +0200

    fetchItems function

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchItems } from '../../store/modules/cartSlice';
 
 function Homepage() {
+  const { items } = useSelector(store => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchItems())
+  }, [dispatch]);
+
   return (
     <>
-      <h1>Homepage</h1>
-      <p>.....</p>
     </>
   );
 }
--- src/store/modules/cartSlice.js
+++ src/store/modules/cartSlice.js
@@ -1,4 +1,10 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const URL = 'https://api.noroff.dev/api/v1/online-shop';
+
+export const fetchItems = createAsyncThunk('fetchItems', () => {
+  return fetch(URL).then((response) => response.json()).catch((error) => console.log(error));
+});
 
 const initialState = {
   items: [],
@@ -9,8 +15,17 @@ const initialState = {
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers: {
+    [fetchItems.fulfilled]: (state, action) => {
+      console.log('action', action);
+      state.items = action.payload;
+    },
+    [fetchItems.rejected]: (state, action) => {
+      console.log('fetch rejected', action);
+    }
+  }
 });
-console.log(cartSlice);
+// console.log('cartSlice', cartSlice);
 
 export default cartSlice.reducer;
