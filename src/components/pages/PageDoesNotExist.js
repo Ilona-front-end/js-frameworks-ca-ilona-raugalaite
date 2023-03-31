2ef9cca2ad84b78e47f24fc2be91b034be067073
Ilona R <ilona.raugalaite@yahoo.co.uk>
2023-03-31 03:13:09 +0200

    add 404 page

--- src/components/pages/PageDoesNotExist.js
+++ src/components/pages/PageDoesNotExist.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
-function PageDoesNotExist() {
-  return (
-    <div>
-      <h1>Page does not exist</h1>
-    </div>
-  );
-}
+// function PageDoesNotExist() {
+//   return (
+//     <div>
+//       <h1>Page does not exist</h1>
+//     </div>
+//   );
+// }
 
-export default PageDoesNotExist;
+// export default PageDoesNotExist;
+
+
+export default function PageDoesNotExist() {
+  return (
+    <>
+      <main className="relative isolate min-h-full">
+        <img
+          src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
+          alt=""
+          className="absolute inset-0 -z-10 h-full w-full object-cover object-top"
+        />
+        <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
+          <p className="text-base font-semibold leading-8 text-white">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">Page not found</h1>
+          <p className="mt-4 text-base text-white/70 sm:mt-6">Something went wrong. We could not find the page you’re looking for.</p>
+        </div>
+      </main>
+    </>
+  )
+}
