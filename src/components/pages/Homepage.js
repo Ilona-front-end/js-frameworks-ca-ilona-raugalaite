f6999024276bc6f507e30bdde65bed9c8cb25056
Ilona R <ilona.raugalaite@yahoo.co.uk>
2023-04-02 20:59:59 +0200

    margin change in homepage

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -36,10 +36,10 @@ export default function Homepage() {
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-              Lets find a gift for you Mom
+            <h1 className="mx-6 text-4xl font-bold tracking-tight text-white sm:text-6xl">
+              Lets find a gift for your Mom
             </h1>
-            <p className="mt-6 text-lg leading-8 text-white">
+            <p className="mx-4 mt-6 text-lg leading-8 text-white">
               Sommer is around the corner and your Mom is looking forward to see You. Lets surprise her and make her smile
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
